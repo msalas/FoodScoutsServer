de5554579142ca0edb68c7d40ba07c1687c0c686
Marc Salas <marc.salas@gmail.com>
2012-05-10 23:01:44 +0300

    Ordered by most popular added to my recommendations raw sql query

--- foodscouts/items/views.py
+++ foodscouts/items/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 
 def recommendations(request,user_id):
    #data = serializers.serialize('json', Item.objects.annotate(average=Avg('review__rating')).exclude(review__user=user_id).filter(Q(average__gt=3) | Q(review__isnull=True)).order_by('-average').distinct('id'),indent=4)
-    data = serializers.serialize('json', Item.objects.raw('select distinct * from items_item ii where ii.id not in (select item_id from items_review where user_id=%s) and (3<=(select avg(rating) from items_review where ii.id=item_id) or ii.id not in (select item_id from items_review))',[user_id]),indent=4)
+    data = serializers.serialize('json', Item.objects.raw('select distinct * from items_item ii where ii.id not in (select item_id from items_review where user_id=%s) and (3<=(select avg(rating) from items_review where ii.id=item_id) or ii.id not in (select item_id from items_review)) order by (select avg(rating) from items_review where ii.id=item_id) desc',[user_id]),indent=4)
     response = HttpResponse(data, mimetype='application/json')
     response['Access-Control-Allow-Origin'] = '*'
     return response
