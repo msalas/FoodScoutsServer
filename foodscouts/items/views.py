711b8ad37d6030f6be50486b5be80d4760bca95f
Marc Salas <marc.salas@gmail.com>
2012-05-10 15:15:04 +0300

    Added 4 entrypoints: recommendations, my_reviews, my_bookmarks, reviews

--- foodscouts/items/views.py
+++ foodscouts/items/views.py
@@ -2,11 +2,29 @@ from django.http import HttpResponse
 from django.core import serializers
 from items.models import *
 
-def index(request):
-#    data = serializers.serialize('json', Review.objects.all(),indent=4,relations=('user','item',))
-#    i = Item.objects.get(id=1)
-
-    data = serializers.serialize('json', Item.objects.all(),indent=4,relations=('review','bookmark',))
+def recommendations(request):
+    data = serializers.serialize('json', Item.objects.all(),indent=4)
     response = HttpResponse(data, mimetype='application/json')
     response['Access-Control-Allow-Origin'] = '*'
     return response
+
+def my_reviews(request,user_id):
+    data = serializers.serialize('json', Review.objects.filter(user=user_id),indent=4,relations=('item'))
+    response = HttpResponse(data, mimetype='application/json')
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
+def my_bookmarks(request,user_id):
+    data = serializers.serialize('json', Bookmark.objects.filter(user=user_id),indent=4,relations=('item'))
+    response = HttpResponse(data, mimetype='application/json')
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
+def reviews(request,item_id):
+    data = serializers.serialize('json', Review.objects.filter(id=item_id),indent=4,relations=('item','user'))
+    response = HttpResponse(data, mimetype='application/json')
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
+	
+	
--- foodscouts/urls.py
+++ foodscouts/urls.py
@@ -6,9 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'items.views.index'),
+     #url(r'^$', 'items.views.index'),
      #url(r'^foodscouts/', include('foodscouts.foo.urls')),
-
+	 url(r'^recommendations/$', 'items.views.recommendations'),
+	 url(r'^my_reviews/(?P<user_id>\d+)/$', 'items.views.my_reviews'),
+	 url(r'^my_bookmarks/(?P<user_id>\d+)/$', 'items.views.my_bookmarks'),
+	 url(r'^reviews/(?P<item_id>\d+)/$', 'items.views.reviews'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
