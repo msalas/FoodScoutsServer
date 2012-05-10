cd54c39e2af0a32fb8666e30886430393933dd32
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-10 10:06:17 +0300

    Added CORS support

--- foodscouts/items/views.py
+++ foodscouts/items/views.py
@@ -4,4 +4,6 @@ from items.models import Item
 
 def index(request):
     data = serializers.serialize('json', Item.objects.all(),indent=4)
-    return HttpResponse(data, mimetype='application/json')
+    response = HttpResponse(data, mimetype='application/json')
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
