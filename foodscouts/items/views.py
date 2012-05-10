d0788294ff2a9fec69f005992a68e0c87fb60495
Marc Salas <marc.salas@gmail.com>
2012-05-10 18:02:20 +0300

    Added items and fixed the filter

--- foodscouts/items/views.py
+++ foodscouts/items/views.py
@@ -2,11 +2,18 @@ from django.http import HttpResponse
 from django.core import serializers
 from items.models import *
 
+
 def recommendations(request):
     data = serializers.serialize('json', Item.objects.all(),indent=4)
     response = HttpResponse(data, mimetype='application/json')
     response['Access-Control-Allow-Origin'] = '*'
     return response
+
+def items(request):
+	    data = serializers.serialize('json', Item.objects.all(),indent=4)
+	    response = HttpResponse(data, mimetype='application/json')
+	    response['Access-Control-Allow-Origin'] = '*'
+	    return response
 
 def my_reviews(request,user_id):
     data = serializers.serialize('json', Review.objects.filter(user=user_id),indent=4,relations=('item'))
@@ -21,10 +28,7 @@ def my_bookmarks(request,user_id):
     return response
 
 def reviews(request,item_id):
-    data = serializers.serialize('json', Review.objects.filter(id=item_id),indent=4,relations=('item','user'))
+    data = serializers.serialize('json', Review.objects.filter(item=item_id),indent=4,relations={'user':{'fields':('username')}})
     response = HttpResponse(data, mimetype='application/json')
     response['Access-Control-Allow-Origin'] = '*'
     return response
-
-	
-	
--- foodscouts/urls.py
+++ foodscouts/urls.py
@@ -6,12 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     #url(r'^$', 'items.views.index'),
+     url(r'^$', 'items.views.items'),
      #url(r'^foodscouts/', include('foodscouts.foo.urls')),
 	 url(r'^recommendations/$', 'items.views.recommendations'),
 	 url(r'^my_reviews/(?P<user_id>\d+)/$', 'items.views.my_reviews'),
 	 url(r'^my_bookmarks/(?P<user_id>\d+)/$', 'items.views.my_bookmarks'),
 	 url(r'^reviews/(?P<item_id>\d+)/$', 'items.views.reviews'),
+    #url(r'^average/(?P<item_id>\d+)/$', 'items.views.average'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
